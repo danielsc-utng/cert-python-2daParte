e227d932b415b0820c494ad3add20fd7ef9c6a25
Luis Daniel Sánchez Cabrera <66752756+danielsc-utng@users.noreply.github.com>
2022-08-11 22:03:57 -0500

    Elaboración laboratorio 3.4.1.12

---
+++ modulo3/lab34112.py
@@ -0,0 +1,23 @@
+## Luis daniel Sánchez Cabrera
+## Laboratorio 3.4.1.12
+class Timer:
+    def __init__( timer ):
+        try:
+            self.__timer = next_second.__timer.index()
+
+    def __str__(self):
+        self.queue.insert(timer)
+        
+    def next_second(self):
+        self.queue.insert(timer)
+
+    def prev_second(self):
+        self.queue.insert(timer)
+
+
+timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
